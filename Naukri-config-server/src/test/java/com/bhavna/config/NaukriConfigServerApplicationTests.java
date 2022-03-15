e296601c678be123188b1ce7ec4fa67f1a179ddf
SaileelaNadendla <Sai.Nadendla@bhavnacorp.com>
2022-03-15 15:36:35 +0530

    Microservices

---
+++ Naukri-config-server/src/test/java/com/bhavna/config/NaukriConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavna.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukriConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
